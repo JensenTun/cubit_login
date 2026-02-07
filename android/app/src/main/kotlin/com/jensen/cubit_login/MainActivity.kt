91d8e347721d51b03bec4d9999a5c2dfbdaaa6fb
JensenTun <tunkyawsoe.rk@gmail.com>
2026-02-07 21:24:32 +0630

    firebase initialize

---
+++ android/app/src/main/kotlin/com/jensen/cubit_login/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jensen.cubit_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cubit_login/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text('Home Page')));
+  }
+}
